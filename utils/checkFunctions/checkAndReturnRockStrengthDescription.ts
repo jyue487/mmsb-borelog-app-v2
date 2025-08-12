7478ace3584dc0bed0b078ff9ba6031401496213
jyue487 <kamjiayue487@gmail.com>
2025-08-13 00:11:54 +0800

    Changed rock description

--- utils/checkFunctions/checkAndReturnRockStrengthDescription.ts
+++ utils/checkFunctions/checkAndReturnRockStrengthDescription.ts
@@ -1,6 +1,8 @@
 export function checkAndReturnRockStrengthDescription(rqdInPercentage: number): string {
     let description: string = '';
-    if (rqdInPercentage <= 50) {
+    if (rqdInPercentage <= 25) {
+        description = 'Weak to moderately strong';
+    } else if (rqdInPercentage <= 50) {
         description = 'Moderately strong';
     } else if (rqdInPercentage <= 75) {
         description = 'Strong';
